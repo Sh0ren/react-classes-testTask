4a3d10a06523aba087af0243abd649565dcea5fc
shore <fromavich1@gmail.com>
2023-06-02 15:00:17 +0300

    second commit

--- src/entites/articleItem/ArticleItem.tsx
+++ src/entites/articleItem/ArticleItem.tsx
@@ -2,9 +2,11 @@ import React from "react"
 import { Article } from "../../../types"
 
 export const ArticleItem: React.FC<Article> = ({ name, review, date }) => {
+  const nameArr = name.split(" ")
+  const formatedName = `${nameArr[0]} ${nameArr[1][0]}.`
   return (
     <div>
-      {name} {review} {date}
+      {formatedName} {review} {date}
     </div>
   )
 }
